b6911fbfbfc573be5b723cc0c2ec3a0bbeb46546
Natanael Alves <natanalvespr@gmail.com>
2022-09-08 14:04:46 -0300

    Primeiro commit

---
+++ Calculadora/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculadora
+//
+//  Created by Natanael Alves Pereira on 08/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+     }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# Calculadora para treinar conceitos
