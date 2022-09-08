c1f5e610c9d11b2792bcfde4f2c0a049a824e17d
Natanael Alves <natanalvespr@gmail.com>
2022-09-08 17:43:38 -0300

    Adicionando numerais

--- Calculadora/ButtonsCollectionViewCell.swift
+++ Calculadora/ButtonsCollectionViewCell.swift
@@ -9,4 +9,45 @@ import UIKit
 
 class ButtonsCollectionViewCell: UICollectionViewCell {
     
+    static let reuseIdentifier = "Cell"
+    
+    static let operadoresNumerais = ["Ac", "Clear", "%" , "/" , "7" , "8" , "9", "x" , "4", "5", "6", "-", "1", "2","3", "+", "", "0", "," , "="]
+    
+    var textLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Teste"
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont(name: "AvenirNext-Medium", size: 20.0) ?? UIFont.systemFont(ofSize: 20.0)
+        label.textColor = .red
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(textLabel)
+        setUpConstraints()
+
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        textLabel.frame = contentView.bounds
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+    }
+    
+    func setUpConstraints() {
+        NSLayoutConstraint.activate([
+            textLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            textLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
--- Calculadora/ButtonsCollectionViewController.swift
+++ Calculadora/ButtonsCollectionViewController.swift
@@ -7,27 +7,25 @@
 
 import UIKit
 
-private let reuseIdentifier = "Cell"
-
 class ButtonsCollectionViewController: UICollectionViewController , UICollectionViewDelegateFlowLayout {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.backgroundColor = .white
-        collectionView.delegate = self
+       
+        self.collectionView.register(ButtonsCollectionViewCell.self, forCellWithReuseIdentifier: ButtonsCollectionViewCell.reuseIdentifier)
         collectionView.dataSource = self
-        self.collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-        view.addSubview(collectionView)
     }
-
-
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 20
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! ButtonsCollectionViewCell
+        cell.textLabel.text = ButtonsCollectionViewCell.operadoresNumerais[indexPath.row]
         cell.backgroundColor = .systemGreen
         return cell
     }
